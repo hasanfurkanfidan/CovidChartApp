167c1e425d6ef95855e15638f6edb95d1b71cb33
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-06-27 21:16:33 +0300

    Veri tabanı oluşturuldu

--- CovidChartApp.Api/CovidContext.cs
+++ CovidChartApp.Api/CovidContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using CovidChartApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CovidChartApp.Api
 {
-    public class CovidContext
+    public class CovidContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS01;Database=CovidChart;Integrated Security=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Covid> Covids { get; set; }
     }
 }
